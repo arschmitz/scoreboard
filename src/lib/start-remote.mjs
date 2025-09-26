7b54c5b3ad5ea0b300370a328ec65df9eff1dbfc
Alexander Schmitz <arschmitz@gmail.com>
2025-09-25 23:53:00 -0400

    update browser spawn

--- src/lib/start-remote.mjs
+++ src/lib/start-remote.mjs
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import waitOn from "wait-on";
-import puppeteer from "puppeteer-core";
 import { promisify } from "node:util";
 import { spawn, exec as execCb } from 'node:child_process';
 import { findChrome } from 'find-chrome-bin';
@@ -51,23 +50,42 @@ async function startServer() {
 
 async function startBrowser() {
   console.log("launching browser");
-  const browser = await puppeteer.launch({
-    headless: false, // Set to false to see the browser window
-    args: [
-      '--start-fullscreen', // Opens in full-screen
-      '--kiosk',           // Enables Chrome's kiosk mode
-      '--app=http://localhost:4173/remote' // to launch a specific URL as an app
-    ],
-    executablePath: chromeInfo.executablePath
+
+  console.log("Starting Server")
+  const { promise, resolve, reject } = Promise.withResolvers();
+
+  const ls = spawn(chromeInfo.executablePath, ['--kiosk', '--new-window', '--start-maximized', '--app=http://localhost:4173/remote']);
+  console.log("Spawned process")
+
+
+  ls.stdout.on('data', (data) => {
+    try {
+      console.log(data.toString())
+      if (/Network/.test(data.toString())) {
+        resolve();
+      }
+    } catch (error) {
+      console.log("resolve bad")
+      console.log(error)
+    }
   });
-  console.log("browser running");
 
-  const page = await browser.newPage();
-  // await page.goto('https://www.example.com'); // Navigate to your desired page
+  ls.stderr.on('data', (data) => {
+    // console.log("ho shit", data.toString());
+  });
 
-  // Keep the browser open for interaction or close it after a certain time
-  // await new Promise(resolve => setTimeout(resolve, 60000)); // Keep open for 60 seconds
-  // await browser.close();
+  ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  try {
+    await promise;
+    console.log("Server Running");
+  } catch {
+    console.log("all bad")
+  }
+
+  return ls;
 }
 
 async function startSpotify() {
